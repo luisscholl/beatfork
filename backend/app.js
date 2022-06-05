6f1077f82bc9e3f5e41d6460efc5cea1adb20385
Lorenz Thiede <lorenz.07.lt@gmail.com>
2022-06-05 14:43:52 +0200

    Implement post and get for levels and add authorization. OrderBy isn't prperly implemented, yet

--- backend/app.js
+++ backend/app.js
@@ -1,45 +1,58 @@
 const express = require('express')
 const { MongoClient } = require('mongodb')
+const OpenApiValidator = require('express-openapi-validator')
+const { CognitoJwtVerifier } = require("aws-jwt-verify")
+const verifyJWT = require('./middleware/jwtVerifier')
+const cors = require('cors')
 
 const app = express()
 const levels = require('./routers/levels')
 const appPort = 443
 
-const mongoHost = 'mongo'
-const mongoPort = '27017'
-const mongoURI = 'mongodb://' + mongoHost + ':' + mongoPort
-const mongoClient = new MongoClient(mongoURI)
+const mongoClient = new MongoClient(process.env.MONGO_URI)
 
+const jwtVerifier = CognitoJwtVerifier.create({
+  userPoolId: process.env.USER_POOL_ID,
+  tokenUse: "id",
+  clientId: process.env.CLIENT_ID
+});
+
+app.use(cors())
 app.use(express.json())
-router.use(
-  OpernApiValidator.middleware({
+app.use(verifyJWT)
+app.use(
+  OpenApiValidator.middleware({
     apiSpec: './docs/openapi.yaml',
-    validateRequests: true,
+    validateRequests: {
+      removeAdditional: "all",
+    },
     validateResponses: false,
   })
 )
 
-app.use('levels', levels)
+app.use('/levels', levels)
 
-app.get('*',function (req, res) {
-    res.status(404).send('Page not found 🦗')
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors
+  })
 })
 
-app.use(function(err, req, res, next) {
-    console.error(err.stack)
-    res.status(err.status || 500).json({
-      message: err.message,
-      errors: err.errors
-    })
-})
-  
-
-mongoClient.connect(function(err, client) {
+mongoClient.connect(function (err, client) {
   if (err) {
     throw err
   }
   app.locals.db = client.db('beatfork')
-  app.listen(appPort, () => {
-    console.log(`Example app listening on port ${appPort}`)
-  })
+  jwtVerifier.hydrate()
+    .catch((err) => {
+      throw err
+    })
+    .then(() => {
+      app.locals.jwtVerifier = jwtVerifier
+      app.listen(appPort, () => {
+        console.log(`Example app listening on port ${appPort}`)
+      })
+    })
 })
