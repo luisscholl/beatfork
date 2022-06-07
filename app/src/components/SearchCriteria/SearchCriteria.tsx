9aa96bee6b4315666b57d639fbc5d8f8d0a5b262
Luis Scholl <mail@luisscholl.de>
2022-06-07 17:58:36 +0200

    styling + start order by UI

--- app/src/components/SearchCriteria/SearchCriteria.tsx
+++ app/src/components/SearchCriteria/SearchCriteria.tsx
@@ -3,6 +3,7 @@ import "./SearchCriteria.scss";
 import { useRecoilState } from "recoil";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckSquare, faSquare } from "@fortawesome/free-solid-svg-icons";
+import { useAuth } from "react-oidc-context";
 import MultiRangeSlider from "../../vendor/components/MultiRangeSlider/MultiRangeSlider";
 import { viewState } from "../../atoms/viewState";
 import { searchCriteriaState } from "../../atoms/searchCriteriaState";
@@ -11,18 +12,21 @@ const SearchCriteria = () => {
   const [view, setView] = useRecoilState(viewState);
   const [searchCriteria, setSearchCritera] =
     useRecoilState(searchCriteriaState);
+  const auth = useAuth();
 
   const wrapperFormatter = useCallback((val: number) => {
     return <div>{val}</div>;
   }, []);
 
   const secondsMinutesFormatter = useCallback((seconds: number) => {
-    return `${Math.round(seconds / 60)}min`;
+    if (seconds === 5 * 60) return "5min+";
+    return `${Math.floor(seconds / 60)}min`;
   }, []);
 
   const secondsMinutesAndSecondsFormatter = useCallback((seconds: number) => {
+    if (seconds === 5 * 60) return <div>∞</div>;
     return (
-      <div>{`${Math.round(seconds / 60)}:${Math.round(seconds % 60)
+      <div>{`${Math.floor(seconds / 60)}:${Math.round(seconds % 60)
         .toString()
         .padStart(2, "0")}`}</div>
     );
@@ -38,6 +42,26 @@ const SearchCriteria = () => {
 
   return (
     <div className="SearchCriteria" data-testid="SearchCriteria">
+      <div className="row">
+        <label htmlFor="order-by">
+          Order By
+          <select name="Order By" id="oder-by">
+            <option value="rating">Rating</option>
+            <option value="difficulty">Difficulty</option>
+            <option value="alphabetical">Alphabetical</option>
+            <option value="length">Length</option>
+            {auth.isAuthenticated && (
+              <option value="personalBest">Personal Best</option>
+            )}
+          </select>
+        </label>
+        <label htmlFor="order-direction">
+          <select name="order-direction">
+            <option value="ascending">Ascending</option>
+            <option value="descending">Descending</option>
+          </select>
+        </label>
+      </div>
       <div className="row">
         Difficulty
         <MultiRangeSlider
@@ -65,8 +89,9 @@ const SearchCriteria = () => {
         Length
         <MultiRangeSlider
           min={0}
-          max={300}
-          step={1}
+          max={5 * 60}
+          /* Note: Step does work like it would be intuitive. It seems like "step" is minimum distance between minValue and maxValue, for which I am using step here. */
+          step={10}
           ruler={false}
           label
           minValue={searchCriteria.minLength}
@@ -86,36 +111,38 @@ const SearchCriteria = () => {
           }
         />
       </div>
-      <div className="row">
-        Personal Best
-        <MultiRangeSlider
-          min={0}
-          max={100}
-          step={1}
-          ruler={false}
-          label
-          minValue={searchCriteria.minPersonalBest}
-          maxValue={searchCriteria.maxPersonalBest}
-          minFormatter={percentageFormatter}
-          maxFormatter={percentageFormatter}
-          minValueFormatter={percentageWrapperFormatter}
-          maxValueFormatter={percentageWrapperFormatter}
-          onChange={(e) =>
-            setSearchCritera((old) => {
-              return {
-                ...old,
-                minPersonalBest: e.minValue,
-                maxPersonalBest: e.maxValue,
-              };
-            })
-          }
-        />
-      </div>
+      {auth.isAuthenticated && (
+        <div className="row">
+          Personal Best
+          <MultiRangeSlider
+            min={0}
+            max={100}
+            /* Note: Step does work like it would be intuitive. It seems like "step" is minimum distance between minValue and maxValue, for which I am using step here. */
+            step={5}
+            ruler={false}
+            label
+            minValue={searchCriteria.minPersonalBest}
+            maxValue={searchCriteria.maxPersonalBest}
+            minFormatter={percentageFormatter}
+            maxFormatter={percentageFormatter}
+            minValueFormatter={percentageWrapperFormatter}
+            maxValueFormatter={percentageWrapperFormatter}
+            onChange={(e) =>
+              setSearchCritera((old) => {
+                return {
+                  ...old,
+                  minPersonalBest: e.minValue,
+                  maxPersonalBest: e.maxValue,
+                };
+              })
+            }
+          />
+        </div>
+      )}
       <div className="row">
         <label
           htmlFor="show-playlists"
           className={searchCriteria.showPlaylists ? "checked" : ""}
-          // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
         >
           <input
             type="checkbox"
